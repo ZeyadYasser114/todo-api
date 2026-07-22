de08ea6368abe6d816d55ed2b31c7750b76a3977
Zeyad Yasser <zeyad2007.4y@gmail.com>
2026-07-22 03:00:24 +0300

    Stage 4: Full CRUD

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.post('/tasks',(req, res) =>{
     const {title} = req.body;
     if (!title || title.trim() == ""){
-        return res.status(400).json({error: "Title is required."});
+        return res.status(400).json({error: "Title is required"});
     }
     const newTask = {
         id: tasks.length + 1,
@@ -16,20 +16,47 @@ app.post('/tasks',(req, res) =>{
     tasks.push(newTask);
     res.status(201).json(newTask);
 });
+
+// ───── Fixed Database ───
 let tasks = [
     {id: 1, title: "Buy the mona lisa", done: false},
     {id: 2, title: "Develop a black hole", done: false},
     {id: 3, title: "Meet abraham linclon",done: false}
 ];
+
+// ─────Update a Task ───
+app.put('/tasks/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const task = tasks.find(t => t.id === id);
+    if (!tasks){return res.status(404).json({error: `Task ${id} not found`});}
+    
+    const {title, done} = req.body;
+    if (title !== undefined && title.trim() === ""){return res.status(400).json({error: 'Task title cannot be empty'});}
+    if(title !== undefined) task.title = title;
+    if (done !== undefined) task.done = done;
+    res.json(tasks);
+});
+
+// ───── Delete Tasks ───
+app.delete('/tasks/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = tasks.findIndex(t => t,id === id);
+    if (index === -1){return res.status(404).json({error: `Task ${id} not found`})}
+    tasks.splice(index,1);
+    res.status(204).send();
+});
 app.get('/', (req, res) => {
     res.json({ name: "Task API", version: "1.0", endpoints: ["/tasks"] });
 });
+
 app.get('/health', (req, res) => {
     res.json({ status: "ok" });
 });
+
 app.get('/tasks', (req, res) => {
     res.json(tasks);
 });
+
 app.get('/tasks/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const task = tasks.find(t => t.id === id);
@@ -38,6 +65,8 @@ app.get('/tasks/:id', (req, res) => {
     }
     res.json(task);
 });
+
+// ───── Listen Message ───
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
